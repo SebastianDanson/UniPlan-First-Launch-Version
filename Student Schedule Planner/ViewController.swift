66259fbc69bc1d10506ff4a0838406667d47a9f1
Sebastian Danson <sebastian.danson@gmail.com>
2020-06-02 12:14:31 -0400

    Initial Commit

---
+++ Student Schedule Planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Student Schedule Planner
+//
+//  Created by Student on 2020-06-02.
+//  Copyright © 2020 Sebastian Danson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
